2e6d27420e682b871f00fd3628949f149bb41d8d
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-07-18 19:07:42 +0300

    add solved task two, delete condition of task one

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsList = document.querySelector('#ingredients');
+
+function createIngredientItem(ingredientName) {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = ingredientName;
+  ingredient.classList.add('item');
+  return ingredient;
+}
+
+const items = ingredients.map(ingredient => createIngredientItem(ingredient));
+
+ingredientsList.append(...items);
