0ed9be1a5abd20a70c349e267ed56fe8c07b36d1
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-07-24 14:30:47 +0300

    add solved task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const colorSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onClickBtn);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickBtn() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
 }
