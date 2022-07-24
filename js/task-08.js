a1769cc966958b5da6a4f0e957434461cdafd326
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-07-24 14:19:23 +0300

    add solved task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
+// const email = loginForm.querySelector('input[name="email"]');
+// const pass = loginForm.querySelector('input[name="password"]');
 
+loginForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  event.preventDefault();
+  if (email.value === '' || password.value === '') {
+    alert('всі поля повинні бути заповнені');
+  } else {
+    console.log({ [email.name]: email.value, [password.name]: password.value });
+    event.currentTarget.reset();
+  }
+}
